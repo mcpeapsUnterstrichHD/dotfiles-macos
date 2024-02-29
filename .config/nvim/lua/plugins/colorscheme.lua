2558b03e010692dfc0ae249f5adbd8e7fdf93c46
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-02-29 13:25:15 +0100

    .

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "AlexvZyl/nordic.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nordic",
+    },
+  },
+}
