9bbc1270d9fc538d896092a146f83e14cf5fca41
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2024-05-12 21:55:34 +0200

    .

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,3 +14,17 @@
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>/Users/mahd/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
     export PATH="$HOME/•Local/bin" :$РАТН
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/anaconda3/bin/conda
+    eval /opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/anaconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
